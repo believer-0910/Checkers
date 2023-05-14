70d5dc40ad519c5b84015a03954e53bed2d7f1ef
Saidamin Qambarov <93591690+believer-0910@users.noreply.github.com>
2023-05-14 12:23:51 +0500

    Add files via upload

---
+++ Java-Checkers/src/model/NetworkPlayer.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ * The {@code NetworkPlayer} class is a dummy player used so that the game
+ * can be updated properly from the corresponding client.
+ */
+public class NetworkPlayer extends Player {
+
+	@Override
+	public boolean isHuman() {
+		return false;
+	}
+
+	/**
+	 * This method does not actually update the game state for network players
+	 * as it is updated when their client sends the updated game.
+	 */
+	@Override
+	public void updateGame(Game game) {}
+
+}
